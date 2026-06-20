721256196c1f1ec559a421a65a39a0b3f7ef3b66
Ricardo Díaz <ric.diazs@duocuc.cl>
2026-06-20 19:17:00 -0400

    docs: agrega documentacion de usuarios
    Agrega documentacion en codigo fuente de usuarios usando sintaxis
    de JSDoc.

--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as usuarioService from "../../../src/service/usuario.service";
 
+/**
+* Este archivo contiene el codigo para la ejecucion de metodos HTTP de /api/usuarios
+* En otras palabras, opera como la capa 'controller' bajo patron MVC (model-view-controller)
+*
+*/
 export const GET = async () => {
+    /**
+    * Metodo GET: Lista todos los usuarios registrados en la base de datos
+    *
+    * @returns Regresa un status code 204 ("no content") si la lista de usuarios esta vacia.
+    *          En caso contrario, regresa objeto JSON con la lista de usuarios registrados y el status code 200 ("ok").
+    *
+    */
     const usuarios = await usuarioService.obtenerUsuarios();
 
     if(!usuarios || usuarios.length === 0) {
@@ -12,6 +24,15 @@ export const GET = async () => {
 };
 
 export const POST = async (req: NextRequest) => {
+    /**
+    * Metodo POST: Crea un usuario
+    *
+    * @param {NextRequest} req: Request (peticion) con body en formato JSON del usuario a registrar
+    *
+    * @returns Un objeto JSON con el usuario registrado junto con el status code 201 ("created").
+    *          Si ocurre algun error, se envia mensaje generado en el codigo o la cadena "Datos inválidos"
+    *          junto con el status code 400 ("bad request").
+    */
     const body = await req.json();
 
     try {
--- src/service/usuario.service.ts
+++ src/service/usuario.service.ts
@@ -1,26 +1,70 @@
 import { Prisma } from '@prisma/client'
 import * as usuarioRepository from '../repository/usuario.repository'
 
+/**
+ * Capa de service para operaciones relacionadas con `Usuario`.
+ *
+ * Encapsula reglas de negocio como "si no existe, lanzar error".
+ */
 export const obtenerUsuarios = async () => {
+  /**
+  * Lista todos los usuarios registrados
+  *
+  * @returns Lista de usuarios registrados 
+  *
+  */
   return await usuarioRepository.getUsuarios()
 }
 
 export const obtenerUsuarioPorId = async (id: number) => {
+  /**
+  * Se obtiene un usuario filtrado por su identificador o 'id'
+  *
+  * @param {number} id - Identificador numerico del usuario
+  *
+  * @returns Regresa la entidad 'Usuario' si es encontrado por su 'id'
+  *
+  * @throws Envia un mensaje de error si el usuario no es encontrado por su id
+  */
   const usuario = await usuarioRepository.getUsuarioById(id)
+
+  // Si el usuario no existe, por el metodo 'getUsuarioById' del repository la variable 'usuario' almacenara el valor 'null'.
+  // En Typescript / Javascript, 'null === false', por lo que '!null === true'.
   if (!usuario) {
     throw new Error(`Usuario con id "${id}" no existe`)
   }
+
   return usuario
 }
 
 export const crearUsuario = async (payload: Prisma.UsuarioCreateInput) => {
+  /**
+  * Crea un usuario a partir de los datos ingresados para su registro
+  *
+  * @param {Prisma.UsuarioCreateInput} payload - El cuerpo de datos del usuario a registrar.
+  *
+  * @returns Regresa la entidad 'Usuario' creada.
+  *
+  */
   return usuarioRepository.createUsuario(payload)
 }
 
 export const eliminarUsuario = async (id: number) => {
+  /**
+  * Elimina un usuario el cual es filtrado por su identificador o 'id'
+  *
+  * @param {number} id - Identificador numerico del usuario a eliminar
+  *
+  * @throws Envia un mensaje de error si el usuario no es encontrado por su 'id'
+  *
+  */
   const usuario = await usuarioRepository.getUsuarioById(id)
+
+  // Si el usuario no existe, por el metodo 'getUsuarioById' del repository la variable 'usuario' almacenara el valor 'null'.
+  // En Typescript / Javascript, 'null === false', por lo que '!null === true'.
   if (!usuario) {
     throw new Error(`Usuario con id "${id}" no existe`)
   }
+
   await usuarioRepository.deleteUsuario(id)
 }
